365f6d08a26abd33746ab1b7d26b980f74fa0c0c
Jina <jinatye@naver.com>
2025-12-12 13:00:20 +0900

    [feat] : 이미지 슬라이더 제작

--- src/components/topSection/TopSection.styled.ts
+++ src/components/topSection/TopSection.styled.ts
@@ -6,6 +6,8 @@ export const Container = styled.section`
   height: 100%;
   padding: 30px 50px;
   overflow: hidden;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const BackgroundImg = styled.img`
@@ -22,5 +24,12 @@ export const BackgroundImg = styled.img`
 export const Content = styled.div`
   position: relative;
   z-index: 1; /* 이미지 위에 올라옴 */
-  color: #000;
+  display: flex;
+  width: 100%;
+  padding: 50px;
+  flex: 1;
+  justify-content: space-between;
+  align-items: center;
 `;
+
+export const Info = styled.p``;
--- src/styles/Colors.ts
+++ src/styles/Colors.ts
@@ -1,9 +1,9 @@
 const Colors = {
   // Main Colors
-  Main10: "#e7eef3", // 메인 오렌지
-  Main20: "#d9e4ec", // 밝은 레드, 알림 색
-  Main30: "#b7c9d3", // 글자3
-  Main40: "#8fa1ad", // 살구색
+  Main10: "#e7eef3",
+  Main20: "#d9e4ec",
+  Main30: "#b7c9d3",
+  Main40: "#8fa1ad",
 };
 
 export default Colors;
